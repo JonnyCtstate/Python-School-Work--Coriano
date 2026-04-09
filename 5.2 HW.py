85d641a515a6b523e38450c110ae9fe13e2cfdb4
JonnyCtstate <jonathan.coriano@ctstate.edu>
2026-04-08 22:07:09 -0400

    Add files via upload

---
+++ 5.2 HW.py
@@ -0,0 +1,14 @@
+berries = ["Strawberry", "Blueberry", "Blackberry", "Cranberry"]
+berries.append("Raspberry")
+print (berries)
+Fruits = []
+Fruits.extend(berries)
+print(Fruits)
+Fruits.insert(2, "Mangoes")
+print(Fruits)
+Fruits.pop(1)
+print(Fruits)
+Fruits.insert(1, "Apples")
+print(Fruits)
+Fruits.sort(key=None, reverse=False)
+print(Fruits)
